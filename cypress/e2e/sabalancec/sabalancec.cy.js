b670c2c92e0dd97b3b58864940a9214841838c88
Daniela Dupuša-Mežgaile <danieladupusa-mezgaile@Danielas-MacBook-Air.local>
2025-03-27 14:03:50 +0200

    finished Navigation scenario

--- cypress/e2e/sabalancec/sabalancec.cy.js
+++ cypress/e2e/sabalancec/sabalancec.cy.js
@@ -5,8 +5,21 @@ describe("Sabalancec", () => {
         beforeEach(() => {
             HomePage.visit();
         });
-        it("Open home page", () => {
-            cy.visit("https://sabalancec-lv.vercel.app/");
+        it("Navigation", () => {
+            // check if logo is visible
+            HomePage.getLogo.should('be.visible');
+            // check if name of website - sabalancec - is visible
+            HomePage.getSiteTitle.should('be.visible');
+            // check if nav has catalogue button
+            HomePage.getCatalogueButton.should('be.visible');
+            // check if nav has bestsellers button
+            HomePage.getBestsellersButton.should('be.visible');
+            // check if nav has allergens button
+            HomePage.getAllergensButton.should('be.visible');
+            // check if nav has contact button
+            HomePage.getContactButton.should('be.visible');
+            // check if nav has signup button
+            HomePage.getSignupButton.should('be.visible');
         });
     });
 });
--- cypress/pageObjects/home.page.js
+++ cypress/pageObjects/home.page.js
@@ -4,4 +4,25 @@ export class HomePage extends BasePage {
     static get url() {
         return "";
     }
+    static get getLogo(){
+        return cy.get('[src="/logo-1.png"]');
+    }
+    static get getSiteTitle(){
+        return cy.get('a').contains('Sabalancec');
+    }
+    static get getCatalogueButton(){
+        return cy.get('nav').contains('CATALOGUE');
+    }
+    static get getBestsellersButton(){
+        return cy.get('nav').contains('BESTSELLERS');
+    }
+    static get getAllergensButton(){
+        return cy.get('nav').contains('ALLERGENS');
+    }
+    static get getContactButton(){
+        return cy.get('nav').contains('CONTACT');
+    }
+    static get getSignupButton(){
+        return cy.get('nav').contains('SIGN UP');
+    }
 }
