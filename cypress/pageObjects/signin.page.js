6fb73f2a03967c0eb9c54fc2e16c4e21a7823361
Daniela Dupuša-Mežgaile <danieladupusa-mezgaile@Danielas-MacBook-Air.local>
2025-03-28 01:28:58 +0200

    added logout scenario

--- cypress/pageObjects/signin.page.js
+++ cypress/pageObjects/signin.page.js
@@ -5,10 +5,10 @@ export class SigninPage extends BasePage {
         return "";
     }
     static get getSigninTab(){
-        return cy.get('[class="w-full rounded-r-xl bg-[#42531C] p-[16px] text-white opacity-25"]').contains('Log in');
+        return cy.get('[class="mb-8 flex w-full"]').contains('Log in');
     }
     static get getLoginBtn(){
-        return cy.get('[class="poppins-bold mt-8 w-full rounded-xl bg-[#42531C] p-2 text-white"]').contains('Log in');
+        return cy.get('[class="poppins-bold mt-8 w-full rounded-xl bg-[#42531C] p-2 text-white hover:cursor-pointer hover:bg-[#5a6e2a] active:bg-[#2e3b14]"]').contains('Log in');
     }
     static get getEmailInput(){
         return cy.get('#email');
@@ -18,5 +18,8 @@ export class SigninPage extends BasePage {
     }
     static get getErr(){
         return cy.get('[class="rounded border border-red-600 bg-red-100 p-2 text-sm text-red-600"]').contains('Invalid credentials.');
+    }
+    static get getSignOutBtn(){
+        return cy.get('[class="bg-red-600 opacity-60 py-2 px-4 rounded-lg text-white cursor-pointer"]').contains('Logout');
     }
 }
